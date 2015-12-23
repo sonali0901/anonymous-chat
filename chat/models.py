354ea2142f553b66a18aa169760df619fcc696c6
sonali0901 <sonaligpt0@gmail.com>
2015-12-23 16:32:47 +0530

    lnmiit_chat deleted

--- chat/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class ChatRoom(models.Model):        
-        name = models.CharField(max_length=100) 
-        def __unicode__(self):
-            return self.name
--- chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
- 
-from chat import views
- 
-urlpatterns = patterns('', 
-	url(r'^$', views.index, name='index'),
-        url(r'^chat_room/', views.chat_room, name='chat_room'),
-       
-)
